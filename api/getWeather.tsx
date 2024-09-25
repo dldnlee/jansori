6b193b7719afbba8141335fe4f868ab3eb39b13d
dldnlee <sungeun8877@gmail.com>
2024-09-25 22:54:18 +0900

    [Fix] - moved api key to environment variable

--- api/getWeather.tsx
+++ api/getWeather.tsx
@@ -3,7 +3,7 @@
 export async function getWeather(lat: number, lon: number) {
   // const url = 'http://apis.data.go.kr/1360000/MidFcstInfoService/getMidFcst?ServiceKey=aqEc3lXo6i5YgxXvi9oM3hJylCRJjg4bPdI9012YD7porVEwqJFBBH0BSBCdywhAtzkgOUZ6pn8UJRVrw03xaA%3D%3D&pageNo=2&numOfRows=10&dataType=JSON&stnId=108&tmFc=202409250600'
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5abb8ac93d9588324344c91a133522a5&units=metric&lang=kr`
+  const url = `${process.env.EXPO_PUBLIC_OPENWEATHER_API_URL}data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.EXPO_PUBLIC_OPENWEATHER_API_KEY}&units=metric&lang=kr`
   try {
     const response = await fetch(url);
     const data = await response.json();
